ec0bd95575da289f62aa9a6c4149e0edb599adcc
nfh-amikom <nurulfhakim@students.amikom.ac.id>
2025-10-06 22:48:48 +0700

    Initial commit

---
+++ praktikum1/aboutme_0213.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Praktikum 1</title>
+</head>
+<body>
+    <div class="header">Praktikum 1</div>
+    <div class="container">
+        <div class="sidebar">
+            <a href="#">Facebook</a>
+            <a href="#">X</a>
+            <a href="#">Linkedin</a>
+            <a href="#">Instagram</a>
+        </div>
+        <div class="content">
+            <h3>About Me</h3>
+            <p>Nama : Nurul Fadhilah Hakim<br>
+                Bidang Keahlian : Memasak, Berenang<br>
+                Hobi : Makan, Tidur<br>
+                <br>
+                Mahasiswa semester 3 dari Universitas Amikom Yogyakarta. <br>
+                Fakultas Ilmu Komputer, Program Studi Bachelor of Information Systems.
+            </p>
+        </div>
+    </div>
+    <div class="footer">
+        Nama : Nurul Fadhilah Hakim | NIM : 24.62.0213 | Kelas : 24 BCIS 01
+    </div>
+</body>
+</html>
